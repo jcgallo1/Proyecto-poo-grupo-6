19c13c60c00ce3d1d2a2517e12b80c8f78a18bc5
jcgallo1 <86312927+jcgallo1@users.noreply.github.com>
2021-06-27 18:34:59 -0500

    Update Participante5k.java

--- Participante5k.java
+++ Participante5k.java
@@ -1,6 +1,10 @@
-
 package modelo;
+public class Participante5k extends Estudiante{
+  private float tiempo;
 
- class Participante5k extends Lectura{
-    private String tiempo;
+  
+  public Participante5k(String id, String nombre,char genero, String carrera, float tiempo){
+    super(id, nombre,genero,carrera);
+    this.tiempo=tiempo;   
+  }
 }
