5de70bc267f0d8513ac69a5e8d1b510ba7c78b98
mcascont <86313097+mcascont@users.noreply.github.com>
2021-06-27 18:35:08 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,27 +1,33 @@
 
 package modelo;
+public class Estudiante extends Participante{
+  protected String carrera;
+  protected char genero;
 
-public class Estudiante {
-    protected String id;
-    protected String nombre;
-    protected char genero;
-    protected String carrera;
+  // constructor Estudiante
+  public Estudiante(String id, String nombre,char genero, String carrera){
+    super(id, nombre);
+    this.genero=genero;
+    this.carrera=carrera;
+  }
 
-    public Estudiante(String id, String nombre, char genero, String carrera) {
-        this.id = id;
-        this.nombre = nombre;
-        this.genero = genero;
-        this.carrera = carrera;
+
+  // Métodos getters 
+  public String getId(){
+      return id;
     }
-
+    public String getNombre(){
+      return nombre;
+    }
+    public char getGenero(){
+      return genero;
+    }
+    public String getCarrera(){
+      return carrera;
+    }
+    // Método toString que retorna el id y nombre del estudiante.
     @Override
     public String toString() {
         return "Estudiante{" + "id=" + id + ", nombre=" + nombre + '}';
-    }
-    public String getnombre(){
-        return this.nombre;
-    }
-    public String getId(){
-        return this.id;
     }
 }
