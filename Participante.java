e8d5685704ae77d91a493ff21f6a87eb643d4962
EdFernandez10 <86313053+EdFernandez10@users.noreply.github.com>
2021-06-27 20:30:04 -0500

    Create Participante.java

---
+++ Participante.java
@@ -0,0 +1,12 @@
+package modelo;
+public class Participante{
+  protected String id;
+  protected String nombre;
+  
+  public Participante(String id, String nombre){
+    this.id=id;
+    this.nombre=nombre;
+  }
+
+
+}
